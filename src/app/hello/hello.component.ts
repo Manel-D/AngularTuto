2f62d0da9c6cac924ca7918bf088f888b053bbab
Manel-D <72567191+Manel-D@users.noreply.github.com>
2021-09-23 00:23:28 +0200

    Commit .2

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HelloComponent implements OnInit {
 
+  movies: any;
+
   constructor() { }
 
   ngOnInit(): void {
+
+    fetch('https://ghibliapi.herokuapp.com/films/?limit=10')
+    .then((response) => response.json())
+    .then((json) => {
+      this.movies = json;
+    });
   }
 
 }
